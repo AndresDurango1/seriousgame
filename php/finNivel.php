18437d9469a1a4f57d246c5714f3f0a443ce8c31
AndresDurango1 <andres.durango1@outlook.com>
2024-10-25 14:07:43 -0500

    ajustes login.php

--- php/finNivel.php
+++ php/finNivel.php
@@ -10,18 +10,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tiempo_transcurrido = $_POST['tiempo_transcurrido'];
     $puntaje = $_POST['puntaje'];
 
-    $stmt = $conexion->prepare("UPDATE niveles_usuarios SET fin = ?, tiempo_transcurrido = ?, puntaje = ?, completado = true WHERE id_usuario = ? AND id_nivel = ?");
-    $stmt->bind_param("ssiii", $fin, $tiempo_transcurrido, $puntaje, $id_usuario, $id_nivel);
+    $stmt1 = $conexion->prepare("SELECT id_nivel_usuario FROM niveles_usuarios WHERE id_usuario = ? AND id_nivel = ? ORDER BY id_nivel_usuario DESC LIMIT 1");
+    $stmt1->bind_param("ii", $id_usuario, $id_nivel);
+    $stmt1->execute();
+    $stmt1->bind_result($id_nivel_usuario);
+    $stmt1->fetch();
+    $stmt1->close();
 
-    if ($stmt->execute()) {
+    $stmt2 = $conexion->prepare("UPDATE niveles_usuarios SET fin = ?, tiempo_transcurrido = ?, puntaje = ?, completado = true WHERE id_nivel_usuario = ?");
+    $stmt2->bind_param("ssii", $fin, $tiempo_transcurrido, $puntaje, $id_nivel_usuario);
+    if ($stmt2->execute()) {
         echo json_encode(["status" => "success", "message" => "Nivel completado registrado con éxito"]);
     } else {
         echo json_encode(["status" => "error", "message" => "Error al registrar la finalización del nivel"]);
     }
-
-    $stmt->close();
-    $conexion->close();
 } else {
     echo json_encode(["status" => "error", "message" => "Método no permitido"]);
 }
-?>
