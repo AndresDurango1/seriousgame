9570734a150451e2f3b116d0a9d5cd7346e6e278
AndresDurango1 <anamariacasta405@gmail.com>
2024-11-19 15:31:44 -0500

    cambios en el formulario inisio de sesion

--- pages/index.php
+++ pages/index.php
@@ -43,7 +43,7 @@
         <!-- MODAL FORMULARIO INICIO DE SESION -->
         <div class="contenedorFormularioInicioSesion" id="contenedorFormularioInicioSesion">
             <div class="contenedorIconoCerrarIS">
-                <i class="far fa-window-close" style="color: #000000;" id="iconoCerrarIS"></i>
+                <i class="far fa-window-close"  id="iconoCerrarIS"></i>
             </div>
             <h1 class="tituloFormularioInicioSesion">Inicio de Sesión</h1>
             <form action="../php/login.php" method="POST" class="fomularioInicioSesion">
@@ -51,10 +51,10 @@
                 <input type="text" name="inputUsuario" id="inputUsuario" required>
                 <label for="inputContrasena">Contraseña</label>
                 <input type="password" name="inputContrasena" id="inputContrasena" required>
-                <button type="submit" href="administrador.php">Iniciar sesión</button>
+                <button class="btn" type="submit" href="administrador.php">Iniciar sesión</button>
             </form>
             <p>
-                <a href="formularioRecuperarContrasena.php">¿Olvidaste tu contraseña?</a>
+                <a style="color: #ffffff;" href="formularioRecuperarContrasena.php">¿Olvidaste tu contraseña?</a>
             </p>
         </div>
         <div class="contenedorSecciones">
