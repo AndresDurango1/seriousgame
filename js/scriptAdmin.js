9e3a0685e5d3ae83587c0f3e6258f0dd142bd746
mariangelu <mariangeluribe0604@gmail.com>
2024-11-29 11:55:19 -0500

    responsive pag admi

--- js/scriptAdmin.js
+++ js/scriptAdmin.js
@@ -7,3 +7,21 @@ menuToggle.addEventListener('click', () => {
     // Alternar la clase 'active' para mostrar u ocultar el menú
     navLinks.classList.toggle('active');
 });
+
+
+    function abrirModal() {
+        document.getElementById('modalActualizarPerfil').style.display = 'block';
+    }
+
+    function cerrarModal() {
+        document.getElementById('modalActualizarPerfil').style.display = 'none';
+    }
+
+    // Cerrar el modal al hacer clic fuera de él
+    window.onclick = function (event) {
+        const modal = document.getElementById('modalActualizarPerfil');
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    };
+
