90656e1c695f1bb3beb6777749b7b5b59c8d09d2
AndresDurango1 <107588830+AndresDurango1@users.noreply.github.com>
2024-12-03 11:00:43 -0500

    ajustrs media query 320-544px en pagina administrador, script del boton menu hamburgesa

--- pages/administrador.php
+++ pages/administrador.php
@@ -73,9 +73,18 @@ $totalPages = ceil($totalUsers / $limit);
 <body>
     <nav class="barraNavegacion">
         <div class="contenedorBotonesRedireccion">
-            <button class="btnRedireccion" onclick="window.location.href='../pages/index.php'">Inicio</button>
-            <button class="btnRedireccion" onclick="window.location.href='../pages/formularioCaracterizacion.php'">Formulario Caracterización</button>
-            <button class="btnRedireccion" onclick="window.location.href='../pages/estadisticas.php'">Ver Estadísticas</button>
+            <button class="btnRedireccion" onclick="window.location.href='../pages/index.php'">
+                Inicio
+                <i class="fas fa-home" id="iconoHome" style="color: #000000;"></i>
+            </button>
+            <button class="btnRedireccion" onclick="window.location.href='../pages/formularioCaracterizacion.php'">
+                Formulario Caracterización
+                <i class="fab fa-wpforms" id="iconoFormularioCaracterizacion" style="color:#000000"></i>
+            </button>
+            <button class="btnRedireccion" onclick="window.location.href='../pages/estadisticas.php'">
+                Ver Estadísticas
+                <i class="fas fa-signal" id="iconoEstadisticas" style="color: #000000;"></i>
+            </button>
         </div>
         <div class="contenedorTitulo">
             <p class="titulo">Las Aventuras de Go</p>
@@ -102,7 +111,12 @@ $totalPages = ceil($totalUsers / $limit);
         </div>
     </nav>
     <div class="contenedorPrincipal">
-        <aside class="barraLateral">
+        <div class="contenedorIconoMenuHamburguesa">
+            <button class="btnMenuHamburguesa" id="btnMenuHamburguesa">
+                <i class="fas fa-bars" id="iconoMenuHamburguesa"></i>
+            </button>
+        </div>
+        <aside class="barraLateral" id="barraLateral">
             <p class="barraLateralTitulo">Actualizar mi perfil</p>
             <div class="contenedorImagenUsuario">
                 <img class="imagenUsuario" src="../recursos/img/imgPerfil/<?php echo $ruta_imagen; ?>" alt="Imagen Usuario">
@@ -128,7 +142,7 @@ $totalPages = ceil($totalUsers / $limit);
                 </form>
             </div>
         </aside>
-        <div class="contenedorPrincipal-content">
+        <div class="contenedorPrincipal-content" id="contenedorPrincipal-content">
             <?php
             // Consulta a la base de datos para obtener los tres mejores puntajes
             $query = "SELECT i.ruta_imagen, u.usuario, SUM(nu.puntaje) AS puntaje_total FROM usuarios u
@@ -209,6 +223,7 @@ $totalPages = ceil($totalUsers / $limit);
         </div>
     </div>
     <script src="../js/scriptAlertas.js"></script>
+    <script src="../js/scriptMenuHamburguesa.js"></script>
 </body>
 
 </html>
